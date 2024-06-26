623defd37af272a28c6a60700ac0443a2fea04d9
JafetMo5601 <jafet.moraugalde15@gmail.com>
2024-06-26 17:57:06 -0600

    refactor

---
+++ data/mongo.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+# from flask import g
+
+import settings as s
+
+class MongoDBClient:
+    _client = None
+
+    @staticmethod
+    def get_client():
+        if MongoDBClient._client is None:
+            MongoDBClient._client = MongoClient(s.MONGO_URI)
+        return MongoDBClient._client
+
+
+def get_db(db_name: str):
+    # if 'db' not in g:
+    #     g.db = MongoDBClient.get_client()[db_name]
+    # return g.db
+    return MongoDBClient.get_client()[db_name]
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.client.close()
